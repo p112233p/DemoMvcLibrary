86f8f4f140fb4c5f8729aa3af602c11605ce31b7
p112233p <shenyen.hsiau@gmail.com>
2018-01-22 13:44:56 +0800

    建立常用可共用的Interface

---
+++ DemoMvc/DemoClassLibrary/Interface/IRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoClassLibrary.Interface
+{
+    public interface IRepository<TEntity> where TEntity : class
+    {
+        void Add(TEntity instance);
+        void Update(TEntity instance);
+        void Delete(TEntity instance);
+        TEntity Get(Expression<Func<TEntity, bool>> predicate);
+        double GetMax(Expression<Func<TEntity, bool>> predicate, Expression<Func<TEntity, double>> predicate2);
+        TEntity GetFist(string conntr);
+        IQueryable<TEntity> GetAll(string conntr);
+    }
+}
